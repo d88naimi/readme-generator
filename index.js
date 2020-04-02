b01d13fb63c0b5b90c6fd64382a08aeb95deda54
David Naimi <d88naimi@gmail.com>
2020-04-01 19:08:50 -0700

    axios and promises are in need to make the render function and refactor the init call

--- index.js
+++ index.js
@@ -3,57 +3,53 @@ const util = require('util');
 const axios = require('axios');
 const inquirer = require('inquirer');
 
-// const writeFileAsync = util.promisify(fs.writeFile);
-// const appendFileAsync = util.promisify(fs.appendFile);
-// const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
+const readFileAsync = util.promisify(fs.readFile);
+
 function promptUser() {
-  inquirer
-    .prompt({
-      message: 'Enter your GitHub username:',
-      name: 'username',
-    })
-    .then(function({ username }) {
-      const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+  return inquirer.prompt({
+    message: 'Enter your GitHub username:',
+    name: 'username',
+  });
+}
+function axiosCall(username) {
+  const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
-      axios.get(queryUrl).then(function(res) {
-        const repoNames = res.data.map(function(repo) {
-          return repo.name;
-        });
-
-        const repoNamesStr = repoNames.join('\n');
-
-        fs.writeFile('readme.md', repoNamesStr, function(err) {
-          if (err) {
-            throw err;
-          }
-
-          console.log(`Saved ${repoNames.length} to the readme.md file`);
-        });
+  axios
+    .get(queryUrl)
+    .then(function(res) {
+      res.data.map(function(repo) {
+        return appendFileAsync('readme.md', `${repo.name}\n`);
       });
+    })
+    .then(function() {
+      return readFileAsync('readme.md', 'utf8');
+    })
+    .then(function(data) {
+      console.log('Saved dad jokes:');
+      console.log(data);
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
-promptUser();
+async function init() {
+  console.log('hi');
+  try {
+    const answers = await promptUser();
 
-// function generateHTML(){
-//   const render = `
-//   # ${}
-//   `;
-//   return render;
-// }
-// async function init() {
-//   console.log('hi');
-//   try {
-//     const answers = await promptUser();
+    const info = await axiosCall(answers);
 
-//     const html = generateHTML(answers);
+    // const html = generateHTML(answers);
 
-//     await writeFileAsync('index.html', html);
+    // await writeFileAsync('index.html', html);
 
-//     console.log('Successfully wrote to index.html');
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
+    console.log('Successfully wrote to readme.md');
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-// init();
+init();
